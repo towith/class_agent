a0a210b54a8bff80c0fec077efaa022965903fd7
niuyh <bullflying@outlook.com>
2020-05-16 17:17:23 +0800

    ...

--- src/HandleAgent.java
+++ src/HandleAgent.java
@@ -8,24 +8,29 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 /**
- *  -javaagent:plugin-agent.jar
+ * -javaagent:plugin-agent.jar
  */
 public class HandleAgent {
+	static String outputDir = "D:\\x_tmp\\wordl\\debug2\\";
+	private static String contained = "XXXX";
+
 	public static void premain(String agentArgs, Instrumentation ins) {
 		System.out.println("--------------javaagent-----------------");
 		ins.addTransformer(new ClassFileTransformer() {
 			@Override
 			public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
-															ProtectionDomain protectionDomain, byte[] classfileBuffer)
-							throws IllegalClassFormatException {
-				if (className.contains("kmss")) {
-					String outputDir = "D:\\company\\xxxx-oa\\debug2\\";
+									ProtectionDomain protectionDomain, byte[] classfileBuffer)
+					throws IllegalClassFormatException {
+				if (className.contains(contained)) {
 					File file = new File(outputDir);
 					File file1 = new File(file, className.substring(0, className.lastIndexOf("/")));
 					if (!file1.exists()) {
 						file1.mkdirs();
 					}
-					File file2 = new File(file1, (className.lastIndexOf("/") >= 0 ? className.substring(className.lastIndexOf("/")).substring(1) : className) + ".class");
+					File file2 = new File(file1,
+							(className.lastIndexOf("/") >= 0 ?
+									className.substring(className.lastIndexOf("/")).substring(1) :
+									className) + ".class");
 					FileOutputStream fileOutputStream = null;
 					try {
 						fileOutputStream = new FileOutputStream(file2);
